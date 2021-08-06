1b8b282b6d30d377f7262905bfa08a4d426e0b0d
vishal8236 <vishalpatidar963027@gmail.com>
2021-08-06 23:07:16 +0530

    create singup routes and function

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,12 +1,20 @@
 class SessionController < ApplicationController
     skip_before_action :verify_authenticity_token
 
-    def create
-        byebug
+    def signup
+        @user = User.new(name: params[:user][:name], email: params[:user][:email],password: params[:user][:password])
+        
+        if @user.save
+            # payload = {user_id: @user.id}
+            # token = encode(payload)
+            render :json => { user_name: params[:user][:name] }
+        else
+            render json:{error: "some error while create user"}
+        end
+        
     end
 
     def login
-        byebug
         user = User.find_by(email: params[:user][:username])
 
         if user 
